98d65a960674b830ab0fba7fccb313dae41892d4
ferk6a <fer@k6a.dev>
2025-01-25 18:27:31 -0300

    Cleanup Attractor

--- Attractor.cs
+++ Attractor.cs
@@ -6,16 +6,6 @@ public partial class Attractor : Area2D
     [Export]
     public float Factor { get; set; } = 50.0f;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-    }
-
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
-
     public override void _PhysicsProcess(double delta)
     {
         foreach (var bubble in GetOverlappingBodies()) {
