939a5199a6130ccce7e3016779d0d7b0b08cff7c
ferk6a <fer@k6a.dev>
2025-01-25 12:05:26 -0300

    Initial physics bubble

---
+++ Attractor.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Attractor : Area2D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        foreach (var bubble in GetOverlappingBodies()) {
+            Vector2 velocity = ((Bubble)bubble).Velocity;
+            velocity += 50.0f * Vector2.Down.Rotated(GetTransform().Rotation);
+            ((Bubble)bubble).Velocity = velocity;
+        }
+    }
+}
--- Bubble.cs
+++ Bubble.cs
@@ -9,8 +9,16 @@ public partial class Bubble : CharacterBody2D
     public override void _PhysicsProcess(double delta)
     {
         Vector2 velocity = Velocity;
-        velocity += GetGravity() * (float)delta;
+        // velocity += GetGravity() * (float)delta;
+        velocity.X = (float)Mathf.Lerp(velocity.X, 160, 0.1);
+        velocity.Y = (float)Mathf.Lerp(velocity.Y, 0, 0.05);
+        if (Mathf.Abs(velocity.Y) < 5) {
+            velocity.Y = 0;
+            var random = new Random();
+            velocity.Y += 50.0f*(random.NextSingle() - 0.5f);
+        }
         Velocity = velocity;
+        GD.Print(velocity.X + " " + velocity.Y);
         MoveAndSlide();
     }
 }
