9c02e403204e7c72e6ea8ed9fe99027380d76e61
ferk6a <fer@k6a.dev>
2025-01-25 15:18:57 -0300

    Factor factor into Factor in Attractor

--- Attractor.cs
+++ Attractor.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Attractor : Area2D
 {
+    [Export]
+    public float Factor { get; set; } = 50.0f;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -17,7 +20,7 @@ public partial class Attractor : Area2D
     {
         foreach (var bubble in GetOverlappingBodies()) {
             Vector2 velocity = ((Bubble)bubble).Velocity;
-            velocity += 100.0f * Vector2.Down.Rotated(GetTransform().Rotation);
+            velocity += Factor * Vector2.Down.Rotated(GetTransform().Rotation);
             ((Bubble)bubble).Velocity = velocity;
         }
     }
