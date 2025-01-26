c11833992264ad61630fa4f42866d9007e560502
ferk6a <fer@k6a.dev>
2025-01-26 02:25:24 -0300

    New wind and fix the shark a bit

--- enemies/Shark.cs
+++ enemies/Shark.cs
@@ -3,82 +3,72 @@ using System;
 
 public partial class Shark : CharacterBody2D
 {
-	[Export] public float MinSpeed = 300.0f;
-	[Export] public float MaxSpeed = 300.0f;
-	
-	[Export] public float MinX = 320.0f;
-	[Export] public float MaxX = 640.0f;
-	
-	enum SharkDirection {
-		LEFT,
-		RIGHT
-	};
-	
-	private float _currentSpeed = 0f;
-	private SharkDirection _direction = SharkDirection.LEFT;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		_currentSpeed = MinSpeed;
-		_direction = SharkDirection.LEFT;
-        //_aware = false;
-        //_attacking = false;
-        //_detectingPlayer = false;
-        //_lastPlayerDetectionTime = 0;
-        //this.Velocity = new Vector2(x: -50, y: 0);
-        //Play("default");
-	}
-	
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		bool increaseSpeed = false;
-		float increaseFactor = 0.0f;
-		increaseSpeed = false;
-		if (increaseSpeed ) {
-			increaseFactor += 0.1f;
-		} else {
-			
-		}
-		_currentSpeed = MinSpeed + (MaxSpeed-MinSpeed) * increaseFactor;
-		
-		if (GlobalPosition.X > MaxX) {
-			_direction = SharkDirection.LEFT;
-			Play("default", _direction);
-		} else if (GlobalPosition.X < MinX) {
-			_direction = SharkDirection.RIGHT;
-			Play("default", _direction);
-		}
-	}
-	
-	public override void _PhysicsProcess(double delta)
-	{
-		//Vector2 position = Vector2.Down.Rotated(GetTransform().Rotation);
-		Vector2 position = (_direction == SharkDirection.RIGHT) ? Vector2.Left : Vector2.Right;
-		
-		Vector2 velocity = _currentSpeed * position;
-		//Vector2 direction = Vector2.Zero;
+    [Export]
+    public float MinSpeed = 300.0f;
+    [Export]
+    public float MaxSpeed = 300.0f;
 
-		Rotation = (_direction == SharkDirection.RIGHT) ? 0 : MathF.PI/2;
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+    [Export]
+    public float MinX = 0.0f;
+    [Export]
+    public float MaxX = 720.0f;
+
+    enum SharkDirection
+    {
+        LEFT,
+        RIGHT
+    };
+
+    private float _currentSpeed = 0.0f;
+    private SharkDirection _direction = SharkDirection.RIGHT;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _currentSpeed = MinSpeed;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        bool increaseSpeed = false;
+        float increaseFactor = 0.0f;
+        increaseSpeed = false;
+        if (increaseSpeed)
+        {
+            increaseFactor += 0.1f;
+        }
+        else
+        {
+
+        }
+        _currentSpeed = MinSpeed + (MaxSpeed - MinSpeed) * increaseFactor;
+
+        if (GlobalPosition.X > MaxX)
+        {
+            _direction = SharkDirection.LEFT;
+            Play("default", _direction);
+        }
+        else if (GlobalPosition.X < MinX)
+        {
+            _direction = SharkDirection.RIGHT;
+            Play("default", _direction);
+        }
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 position = (_direction == SharkDirection.LEFT) ? Vector2.Left : Vector2.Right;
+        Vector2 velocity = _currentSpeed * position;
+        Velocity = velocity;
+        MoveAndSlide();
+    }
 
 
     private void Play(string animationName, SharkDirection direction)
     {
         var animPlayer = GetNode<AnimatedSprite2D>("SharkAnimation");
-        if (animPlayer != null)
-        {
-			animPlayer.Play(animationName);
-			if (direction == SharkDirection.RIGHT) {
-            	animPlayer.FlipH = false;
-			} else {
-				animPlayer.FlipH = true;
-			}
-        }
+        animPlayer.Play(animationName);
+        animPlayer.FlipH = direction == SharkDirection.LEFT;
     }
-	
-	
 }
--- objects/Attractor.cs
+++ objects/Attractor.cs
@@ -1,39 +1,42 @@
 using Godot;
 using System;
+using Godot.Collections;
 
 public partial class Attractor : Area2D
 {
     [Export]
     public float Factor { get; set; } = 50.0f;
 
-    private PackedScene _WindScene = GD.Load<PackedScene>("res://wind.tscn");
+    private Array<Polygon2D> _winds = new();
+
+    private Array<int> _nextWinds = new();
 
     // Called by WindTimer
     public void WindSpawn()
     {
-        var WindLocationA = GetNode<Marker2D>("WindLocationA");
-        var WindLocationB = GetNode<Marker2D>("WindLocationB");
-
-        var WindLocation = new Vector2(
-            x: (float)Mathf.Lerp(WindLocationA.Position.X, WindLocationB.Position.X, GD.Randf()),
-            y: (float)Mathf.Lerp(WindLocationA.Position.Y, WindLocationB.Position.Y, GD.Randf())
-        );
-
-        var wind = _WindScene.Instantiate<Sprite2D>();
-        wind.Position = WindLocation;
-        wind.Rotation = Rotation - 80;
-        wind.Scale = new Vector2(
-            x: wind.Scale.X + GD.Randf() * 10,
-            y: wind.Scale.Y
-        );
-
-        if (GD.Randf() > 0.5)
-        {
-            wind.SetFlipH(true);
-            wind.SetFlipV(false);
+        var collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+        var rectangleSize =  ((RectangleShape2D)collisionShape.Shape).Size / 2;
+        var topLeft = collisionShape.Position - rectangleSize;
+        var bottomRight = collisionShape.Position + rectangleSize;
+        if (_nextWinds.Count == 0) {
+            for (int i = 0; i < 16; i++) {
+                _nextWinds.Add(i);
+            }
         }
+        var random = _nextWinds.PickRandom();
+        var windLocation = new Vector2(
+            x: (float)Mathf.Lerp(topLeft.X, bottomRight.X, random / 16.0f),
+            y: (float)Mathf.Lerp(topLeft.Y, topLeft.Y + 10, GD.Randf())
+        );
+        _nextWinds.Remove(random);
 
-        AddChild(wind);
+        var polygon = new Polygon2D
+        {
+            Polygon = new Vector2[] { new(0, 0), new(4, 0), new(4, 120), new(0, 120) },
+            Position = windLocation,
+        };
+        _winds.Add(polygon);
+        AddChild(polygon);
     }
 
     public override void _PhysicsProcess(double delta)
@@ -48,6 +51,23 @@ public partial class Attractor : Area2D
             Vector2 velocity = ((Bubble)bubble).Velocity;
             velocity += Factor * Vector2.Down.Rotated(rotation);
             ((Bubble)bubble).Velocity = velocity;
+        }
+
+        var collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+        var rectangleSize =  ((RectangleShape2D)collisionShape.Shape).Size / 2;
+        var bottomRight = collisionShape.Position + rectangleSize;
+
+        Array<Polygon2D> toRemove = new();
+        foreach (var wind in _winds) {
+            wind.Position += Factor * 10.0f * Vector2.Down * (float)delta;
+            if (wind.Position.Y+120 > bottomRight.Y) {
+                toRemove.Add(wind);
+            }
+        }
+
+        foreach(var wind in toRemove) {
+                wind.QueueFree();
+            _winds.Remove(wind);
         }
     }
 }
