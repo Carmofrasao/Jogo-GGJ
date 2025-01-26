c423a45d07617984271e26a0cdbac60bdc951e35
xnep1 <xnep1@hotmail.com>
2025-01-26 10:40:41 -0300

    The new bubble animation is now played

--- Bubble.cs
+++ Bubble.cs
@@ -4,29 +4,49 @@ using System.Net.Http;
 
 public partial class Bubble : CharacterBody2D
 {
-    [Export]
-    public float TargetX = 0f;
+	[Export]
+	public float TargetX = 0f;
 
-    [Export]
-    public float TargetY = 0f;
+	[Export]
+	public float TargetY = 0f;
 
-    [Signal]
-    public delegate void HitEventHandler();
+	[Signal]
+	public delegate void HitEventHandler();
 
-    public override void _PhysicsProcess(double delta)
-    {
-        Vector2 velocity = Velocity;
-        velocity.X = (float)Mathf.Lerp(velocity.X, TargetX, 0.1);
-        velocity.Y = (float)Mathf.Lerp(velocity.Y, TargetY, 0.05);
-        if (Mathf.Abs(velocity.Y) < 5)
-        {
-            velocity.Y = 0;
-            velocity.Y += 50.0f * (GD.Randf() - 0.5f);
-        }
-        Velocity = velocity;
-        MoveAndSlide();
-        for (int i = 0; i < GetSlideCollisionCount(); i++) {
-            EmitSignal(SignalName.Hit);
-        }
-    }
+	[Signal]
+	public delegate void BurstAnimationFinishedEventHandler();
+
+	public override void _Ready()
+	{
+		var _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		_animationPlayer.Play("default");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+		velocity.X = (float)Mathf.Lerp(velocity.X, TargetX, 0.1);
+		velocity.Y = (float)Mathf.Lerp(velocity.Y, TargetY, 0.05);
+		if (Mathf.Abs(velocity.Y) < 5)
+		{
+			velocity.Y = 0;
+			velocity.Y += 50.0f * (GD.Randf() - 0.5f);
+		}
+		Velocity = velocity;
+		MoveAndSlide();
+		for (int i = 0; i < GetSlideCollisionCount(); i++) {
+			EmitSignal(SignalName.Hit);
+		}
+	}
+	
+	public void Burst(){
+		var _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		_animationPlayer.Play("burst");
+		// burst animation calls _FinishBurst()
+	}
+	
+	private void _FinishBurst(){
+		Hide();
+		EmitSignal(SignalName.BurstAnimationFinished);
+	}
 }
--- Main.cs
+++ Main.cs
@@ -3,23 +3,28 @@ using System;
 
 public partial class Main : Node2D
 {
-    [Export]
-    public PackedScene firstScene;
+	[Export]
+	public PackedScene firstScene;
 
-    private Node _loadedScene;
+	private Node _loadedScene;
 
-    public void StartGame()
-    {
-        GetNode<Hud>("HUD").HideTitleScreen();
-        GetNode<Bubble>("Bubble").Position = new Vector2(0, 0);
-        GetNode<Bubble>("Bubble").TargetX = 100.0f;
-        _loadedScene = firstScene.Instantiate();
-        AddChild(_loadedScene);
-    }
+	public void StartGame()
+	{
+		GetNode<Hud>("HUD").HideTitleScreen();
+		GetNode<Bubble>("Bubble").Position = new Vector2(0, 0);
+		GetNode<Bubble>("Bubble").TargetX = 100.0f;
+		_loadedScene = firstScene.Instantiate();
+		AddChild(_loadedScene);
+	}
 
-    public void GameOver()
-    {
-        GetNode<Hud>("HUD").ShowTitleScreen();
-        _loadedScene.QueueFree();
-    }
+	public void GameOver()
+	{
+		var bubble = GetNode<Bubble>("Bubble");
+		bubble.Burst();
+	}
+	
+	private void Reset(){
+		GetNode<Hud>("HUD").ShowTitleScreen();
+		_loadedScene.QueueFree();
+	}
 }
