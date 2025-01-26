533e3b177a5acd4ec54e7769e433d344f2b015ea
Muriki <mgy20@inf.ufpr.br>
2025-01-26 02:02:36 -0300

    Added more space objects

--- KeyUpDownAnimationTimer.cs
+++ KeyUpDownAnimationTimer.cs
@@ -3,18 +3,18 @@ using System;
 
 public partial class KeyUpDownAnimationTimer : Timer
 {
-    [Export]
-    public int Frame = 0;
+	[Export]
+	public int Frame = 0;
 
-    [Export]
-    public int FrameCount = 5;
+	[Export]
+	public int FrameCount = 5;
 
-    public void OnTimeout()
-    {
-        foreach (var animatedSprite in GetTree().GetNodesInGroup("AnimatedKeys"))
-        {
-            ((AnimatedSprite2D)animatedSprite).Frame = Frame;
-        }
-        Frame = (Frame + 1) % FrameCount;
-    }
+	public void OnTimeout()
+	{
+		foreach (var animatedSprite in GetTree().GetNodesInGroup("AnimatedKeys"))
+		{
+			((AnimatedSprite2D)animatedSprite).Frame = Frame;
+		}
+		Frame = (Frame + 1) % FrameCount;
+	}
 }
--- objects/Planet.cs
+++ objects/Planet.cs
@@ -3,31 +3,31 @@ using System;
 
 public partial class Planet : Area2D
 {
-    [Export]
-    public float Factor { get; set; } = 30.0f;
+	[Export]
+	public float Factor { get; set; } = 30.0f;
 
-    public override void _Process(double delta)
-    {
-        if (Input.IsActionPressed("ui_up"))
-        {
-            Factor = (float)Mathf.Clamp(Factor + 100 * delta, -30, 30);
-        }
-        if (Input.IsActionPressed("ui_down"))
-        {
-            Factor = (float)Mathf.Clamp(Factor - 100 * delta, -30, 30);
-        }
-    }
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionPressed("ui_up"))
+		{
+			Factor = (float)Mathf.Clamp(Factor + 100 * delta, -30, 30);
+		}
+		if (Input.IsActionPressed("ui_down"))
+		{
+			Factor = (float)Mathf.Clamp(Factor - 100 * delta, -30, 30);
+		}
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        foreach (var body in GetOverlappingBodies())
-        {
-            if (body is Bubble bubble)
-            {
-                Vector2 velocity = bubble.Velocity;
-                velocity += Factor * (this.GlobalPosition - bubble.GlobalPosition).Normalized();
-                bubble.Velocity = velocity;
-            }
-        }
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		foreach (var body in GetOverlappingBodies())
+		{
+			if (body is Bubble bubble)
+			{
+				Vector2 velocity = bubble.Velocity;
+				velocity += Factor * (this.GlobalPosition - bubble.GlobalPosition).Normalized();
+				bubble.Velocity = velocity;
+			}
+		}
+	}
 }
