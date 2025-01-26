ba748698f291c812efba4ec5910a3f7cdb7c0231
ferk6a <fer@k6a.dev>
2025-01-26 03:08:16 -0300

    Add concentric circles for gravity

--- objects/Planet.cs
+++ objects/Planet.cs
@@ -16,7 +16,22 @@ public partial class Planet : Area2D
 		{
 			Factor = (float)Mathf.Clamp(Factor - 100 * delta, -30, 30);
 		}
+		QueueRedraw();
 	}
+
+	private const int _rate = 3000;
+	private float _elapsed = 0.0f;
+
+    public override void _Draw()
+    {
+		for (int i = 0; i < 4; i++)
+		{
+			var current = ((_elapsed + _rate*i/4.0f) % _rate + _rate) % _rate / (float)_rate;
+			var color = new Color(1f, 1f, 1f, 0.5f*current);
+			DrawCircle(new Vector2(0, 0), Mathf.Lerp(800.0f, 0.0f, current), color, false, 10.0f, true);
+		}
+		_elapsed += Factor;
+    }
 
 	public override void _PhysicsProcess(double delta)
 	{
